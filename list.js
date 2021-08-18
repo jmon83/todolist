b216b9e760f7ecec4281df5e83de022a2f1064b6
jmon83 <76069447+jmon83@users.noreply.github.com>
2021-08-17 21:33:29 -0400

    update

--- list.js
+++ list.js
@@ -1,26 +1,26 @@
  document.addEventListener('DOMContentLoaded', 
- function() {
+ function () {
 
-     var theList = documnent.getElementById('theList');
+     var theList = document.getElementById('theList');
      var theInput = document.getElementById('theInput');
      var addButton = document.getElementById('addButton');
      var theCount = 0;
 
      var addToDoItem = function() {
          var toDoCol = document.createElement('div');
-         toDoCol.setAttribute('class', 'col-sm-8 thingsToDo');
+         toDoCol.setAttribute('class', 'col-xs-12 thingsToDo');
+         var toDoRow = document.createElement('div');
          toDoRow.setAttribute('class', 'row');
-         var removeToDo = document.createElement('button');
+         var removeButton = document.createElement('button');
          removeButton.setAttribute('class', 'btn btn-danger remove-button');
-         removeButton.innerHTML = "Done!";
-
-         removeButton.onClick = function() {
+         removeButton.innerHTML = "X";
+         removeButton.onclick = function () {
              var child = this.parentNode.parentNode;
              theList.removeChild(child);
          };
 
     var h5 = document.createElement('h5');
-    h5.setAttribute('class', 'col-sm-6');
+    h5.setAttribute('class', 'col-xs-8');
     h5.innerHTML = theInput.value;
     toDoRow.appendChild(h5);
     toDoRow.appendChild(removeButton);
@@ -28,7 +28,7 @@
     theList.appendChild(toDoCol);
      };
 
-    var checkTheToDo = function() {
+    var checkTheToDo = function () {
         if (theCount < 10 && theInput.value !== '') {
             addToDoItem();
             theCount++;
@@ -37,7 +37,7 @@
     }
     addButton.addEventListener('click', checkTheToDo);
 
-    theInput.addEventListener('keyup', function(event) {
+    theInput.addEventListener('keyup', function (event) {
         if (event.key === "Enter") {
             checkTheToDo();
         }
